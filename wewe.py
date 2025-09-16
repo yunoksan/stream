c7927581a87fedc2d60a0273d6457aabf08d6368
윤옥산 <157436357+yunoksan@users.noreply.github.com>
2025-09-16 21:58:51 +0900

    Update wewe.py

--- wewe.py
+++ wewe.py
@@ -1,10 +1,7 @@
 import streamlit as st
-from pyzbar.pyzbar import decode
-from PIL import Image
 import requests
 import base64
 
-st.title("📷 바코드 / QR 코드 인식")
 API_KEY = "YOUR_GOOGLE_CLOUD_API_KEY"
 url = f"https://vision.googleapis.com/v1/images:annotate?key={API_KEY}"
 
@@ -13,14 +10,6 @@ st.title("📷 바코드 스캐너 (Google Vision API)")
 img_file = st.camera_input("코드를 카메라로 찍어주세요")
 
 if img_file:
-    img = Image.open(img_file)
-    decoded = decode(img)
-
-    if decoded:
-        for d in decoded:
-            st.success(f"인식된 코드: {d.data.decode('utf-8')}")
-    else:
-        st.error("❌ 코드를 인식하지 못했습니다.")
     content = base64.b64encode(img_file.getvalue()).decode("utf-8")
     body = {
         "requests": [
